6c53859fc6c8aa903eff9e6b8d2be218be735415
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2023-12-15 08:41:14 -0800

    Add files via upload

--- main/src/lexer.py
+++ main/src/lexer.py
@@ -5,13 +5,13 @@ import error
 def tokenize(string):
   n = -1
   tokens = [""]
-  while not n == len(string)-1:
+  while n != len(string)-1:
     n += 1
     if not string[n] in ["  ", " ", "+", "-", "*", "/"]:
       if string[n] == "\"":
         n += 1
         tokens.append("")
-        while not string[n] == "\"":
+        while string[n] != "\"":
           tokens[len(tokens)-1] = f"{tokens[len(tokens)-1]}{string[n]}"
           n += 1
         tokens[len(tokens)-1] = f"\"{tokens[len(tokens)-1]}\""
@@ -47,27 +47,30 @@ def parse(tokens):
             else:
               nodes[f"obj{len(nodes)+1}"] = "error1"
           else:
-            nodes[f"obj{len(nodes)+1}"] = {"type":"float", "contents":token, "len":len(token)-2}
+            nodes[f"obj{len(nodes)+1}"] = {"type":"float", "contents":token, "len":len(token)}
         except ValueError:
           if token in ["+", "-", "*", "/"]:
             nodes[f"obj{len(nodes)+1}"] = {"type":"operator", "contents":token}
           else:
             nodes[f"obj{len(nodes)+1}"] = "error1"
         else:
-          nodes[f"obj{len(nodes)+1}"] = {"type":"int", "contents":token, "len":len(token)-2}
-        if nodes[f"obj{len(nodes)}"]["type"] in ["int", "float"]:
-          if len(nodes) > 2:
-            if nodes[f"obj{len(nodes)-1}"]["type"] == "operator":
-              n = 0
-              c = len(nodes)-3
-              newnodes = {}
-              while n != c:
-                n += 1
-                newnodes[f"obj{n}"] = nodes[f"obj{n}"]
-              newnodes[f"obj{len(newnodes)+1}"] = {"type":"operator", "contents":nodes[f"obj{len(nodes)-1}"]["contents"],  "operation":{"obj1":nodes[f"obj{len(nodes)-2}"], "obj2":nodes[f"obj{len(nodes)}"]}}
-              n += 2
-              while n != len(nodes)-1:
-                n += 1
-                newnodes[f"obj{n-3}"] = nodes[f"obj{n}"]
-              nodes = newnodes
+          nodes[f"obj{len(nodes)+1}"] = {"type":"int", "contents":token, "len":len(token)}
+          
+
+        if not "error" in nodes[f"obj{len(nodes)}"]:
+          if nodes[f"obj{len(nodes)}"]["type"] in ["int", "float"]:
+            if len(nodes) > 2:
+              if nodes[f"obj{len(nodes)-1}"]["type"] == "operator":
+                n = 0
+                c = len(nodes)-3
+                newnodes = {}
+                while n != c:
+                  n += 1
+                  newnodes[f"obj{n}"] = nodes[f"obj{n}"]
+                newnodes[f"obj{len(newnodes)+1}"] = {"type":"operator", "contents":nodes[f"obj{len(nodes)-1}"]["contents"],  "operation":{"obj1":nodes[f"obj{len(nodes)-2}"], "obj2":nodes[f"obj{len(nodes)}"]}}
+                n += 2
+                while n != len(nodes)-1:
+                  n += 1
+                  newnodes[f"obj{n-3}"] = nodes[f"obj{n}"]
+                nodes = newnodes
   return nodes
