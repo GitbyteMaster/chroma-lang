8d49e9460db4e91a74d480c416f19f135cf305e5
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2023-12-15 08:40:22 -0800

    Add files via upload

--- main/main.py
+++ main/main.py
@@ -4,18 +4,19 @@ sys.path.append("main/src")
 import error
 import lexer
 
-file = ""
+file = "hi+\"hi\"+2 ;"
 script = file.split(";")
 if script == ['']:
   error.FatalError("None", "File is empty", True)
 else:
   n = -1
   lasterr = []
-  while not n == len(script)-1:
+  while n != len(script)-1:
     n += 1
     if script[n] != "":
       parsed = lexer.parse(script[n])
-      print(parsed)
+
+      # Check parsed line
       for x in parsed:
         if "error" in parsed[x]:
           parent = False
@@ -35,11 +36,29 @@ else:
           except KeyError:
             parent = False
             if lasterr == []:
-              lasterr.append("error2")
+              lasterr.append("2")
               lasterr.append(n)
               parent = True
             elif lasterr[1] != n:
-              lasterr[0] = "error2"
+              lasterr[0] = "2"
               lasterr[1] = n
               parent = True
-            error.SyntaxError(str(n+1), f"Operator \''{parsed[x]['contents']}\' missing \'int\' or \'float\'.", parent)
+            error.SyntaxError(str(n+1), f"Operator \'{parsed[x]['contents']}\' missing \'int\' or \'float\'.", parent)
+          else:
+            contents = parsed[x]["operation"]["obj1"]
+            try:
+              parsed[x]["operation"]["obj1"]["type"]
+            except TypeError:
+              error.SyntaxError(str(n+1), f"Invalid syntax.", parent)
+            else:
+              if not parsed[x]["operation"]["obj1"]["type"] in ["int", "float", "operator"]:
+                parent = False
+                if lasterr == []:
+                  lasterr.append("2")
+                  lasterr.append(n)
+                  parent = True
+                elif lasterr[1] != n:
+                  lasterr[0] = "2"
+                  lasterr[1] = n
+                  parent = True
+                error.SyntaxError(str(n+1), f"Expected \'int\' or \'float\', not \'{parsed[x]['operation']['obj1']['type']}\'.", parent)
