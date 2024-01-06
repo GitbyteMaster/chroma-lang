ec7fb78cec0d5ef320ace780b61a1da9fa7c3dc3
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2024-01-06 09:34:20 -0800

    Add files via upload

---
+++ main/src/tokens (1).py
@@ -0,0 +1,21 @@
+STR = "STR"
+# String
+INT = "INT"
+# Integer
+FLOAT = "FLOAT"
+# Floating Point Number
+BOOL = "BOOL"
+# Boolean
+
+LPAREN = "LPAREN"
+# (
+RPAREN = "RPAREN"
+# )
+LBRACE = "LBRACE"
+# {
+RBRACE = "RBRACE"
+# }
+COMMA = "COMMA"
+# ,
+OPER = "OPER"
+# +, -, *, /, =, ^, <, >, ==, !=, &&,
